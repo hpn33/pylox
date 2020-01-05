e631a7ac9382b97bf56acc4464c6e395ede2408c
hpn33 <hamed.hpn332@gmail.com>
2020-01-05 20:28:10 +0330

    something by pycharm

--- scanner.py
+++ scanner.py
@@ -6,10 +6,14 @@ class scanner:
 	def __init__(self, lines = []):
 		self.tokens = []
 		
-		self.get_tokens(lines)
+		self.set_lines(fr)
 	
-	def set_lines(self, lines = []):
-		self.get_tokens(lines)
+	def set_lines(self, fr):
+		
+		self.tokens = []
+		
+		
+		self.get_tokens(fr.lines)
 	
 	def get_tokens(self, lines):
 		for line in lines:
@@ -18,30 +22,27 @@ class scanner:
 			self.set_tokens(line)
 		
 		for tok in self.tokens:
-			print(tok.type[:6],'\t', tok.source)
+			print(tok.type[:6], '\t', tok.source)
 	
-
 	def set_tokens(self, line):
 		text = ''
 		
 		counter = 0
 		
-		while counter <= len(line)-1:
+		while counter <= len(line) - 1:
 			
 			w = line[counter]
-		
 			
 			if w in [' ', '\t']:
 				text = ''
 				counter += 1
 				continue
 			
-			
 			text += w
-			#print(w, ' ', text)
+			# print(w, ' ', text)
 			
 			if text in util.keyword:
-				tok = util.token(text, 'keyword')
+				tok = util.Token(text, 'keyword')
 				self.tokens.append(tok)
 				text = ''
 			
@@ -49,82 +50,74 @@ class scanner:
 				text = ''
 			
 			if text in util.assignment:
-				tok = util.token(text, 'assign')
+				tok = util.Token(text, 'assign')
 				self.tokens.append(tok)
 				text = ''
 			
 			if text in ["\'", '\"']:
-				callback = self.string(line,counter)
+				callback = self.string_action(line, counter)
 				counter = callback['counter']
 				text = callback['string']
 				
-				tok = util.token(text, 'string')
+				tok = util.Token(text, 'string')
 				self.tokens.append(tok)
 				text = ''
 				continue
 			
 			counter += 1
-		
-	def string(self, line, counter):
+	
+	def string_action(self, line, counter):
 		string = ''
 		is_string = False
-		
 		counter = counter
+		
 		while counter <= len(line):
 			w = line[counter]
 			
 			if is_string:
-				if w in ['\'', '\"']:
+				if self.check_mark(w):
 					is_string = False
 					counter += 1
 					break
+			
 			else:
-				if w in ['\'', '\"']:
+				if self.check_mark(w):
 					is_string = True
 					counter += 1
 					continue
-				
 			
-			string += w		
+			string += w
 			counter += 1
 		
 		return {'counter': counter, 'string': string}
+	
+	def check_mark(self, word):
+		return word in ['\'', '\"']
+	
 
 
-class file_reader:
+class FileReader:
 	
 	def __init__(self, path):
 		self.lines = []
+		self.length = 0
 		
 		self.set_file_name(path)
-
+	
 	def set_file_name(self, file_name):
 		f = open(file_name, 'r')
 		
 		self.set_file(f)
 		
-		f.close()	
+		f.close()
 	
 	def set_file(self, file):
-	
+		self.length = len(file.read())
+		
 		count = 1
 		for line in file:
-			
-			lin = util.line(count, line)
+			lin = util.Line(count, line)
 			self.lines.append(lin)
 			
 			del lin
 			count += 1
-			
-
-
-
-
-
-
-
-
-
-
-
-
