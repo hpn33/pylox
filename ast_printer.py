3d6a38d93846ede662b7546fe090e39eca174ec7
hpn33 <hamed.hpn332@gmail.com>
2020-01-10 22:01:05 +0330

    c5.4 a (not very) pretty printer

---
+++ ast_printer.py
@@ -0,0 +1,45 @@
+from grammar.Expr import *
+from token import TokenType, Token
+
+
+class AstPrinter:
+	
+	def print(self, expr: Expr) -> str:
+		return expr.accept(self)
+	
+	def parenthesize(self, name: str, *exprs) -> str:
+		string = f'({name}'
+		for expr in exprs:
+			string += ' '
+			string += expr.accept(self)
+		
+		string += ')'
+		
+		return string
+	
+	def visit_binary_expr(self, expr: Binary) -> str:
+		return self.parenthesize(expr.operator.lexeme, expr.left, expr.right)
+	
+	def visit_grouping_expr(self, expr: Grouping) -> str:
+		return self.parenthesize('group', expr.expression)
+	
+	def visit_literal_expr(self, expr: Literal) -> str:
+		if expr.value is None:
+			return 'nil'
+		
+		return f'{expr.value}'
+	
+	def visit_unary_expr(self, expr: Unary) -> str:
+		return self.parenthesize(expr.operator.lexeme, expr.right)
+
+
+if __name__ == '__main__':
+	expression = Binary(
+		Unary(
+			Token(TokenType.MINUS, '-', None, 1),
+			Literal(123)),
+		Token(TokenType.STAR, '*', None, 1),
+		Grouping(Literal(45.67))
+	)
+	
+	print(AstPrinter().print(expression))
