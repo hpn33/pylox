dd47f64868318c6489d0f81a88c123e625bab7f0
hpn33 <hamed.hpn332@gmail.com>
2020-01-14 23:26:44 +0330

    c7.2.4

--- oylox/core/Interpreter.py
+++ oylox/core/Interpreter.py
@@ -1,6 +1,7 @@
 from abc import ABC
 
 from oylox.grammar.Expr import ExprVisitor, Expr, Grouping, Unary
+from oylox.util.token_type import TokenType
 
 
 class Interpreter(ExprVisitor, ABC):
@@ -10,12 +11,24 @@ class Interpreter(ExprVisitor, ABC):
 	
 	def visit_unary_expr(self, expr: Unary):
 		right = self.evaluate(expr.right)
+		
+		if expr.operator.typ == TokenType.MINUS:
+			return -right
+		elif expr.operator.typ == TokenType.BANG:
+			return not self.is_truthy(right)
+		
+		return None
 	
+	def is_truthy(self, object):
+		if object is None:
+			return False
+		if object is bool:
+			return bool(object)
+		
+		return True
 	
 	def visit_grouping_expr(self, expr: Grouping):
 		return self.evaluate(expr.expression)
-	
-	
 	
 	def evaluate(self, expr: Expr):
 		return expr.accept(self)
